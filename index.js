62f3746bbce4b80e90300acd87750ec719fe99f4
Amiodas <amio.das.dip@gmail.com>
2023-08-04 22:44:15 +0100

    #delete toy, #edit toy, #filter data, #jwt implement,  #verify data with jwt

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ const client = new MongoClient(uri, {
 
 const verifyJWT = (req, res, next) => {
   console.log("verify jwt");
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, errorMessage: "Unauthorize Access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, errorMessage: "Unauthorized Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 };
 
 async function run() {
@@ -37,6 +53,16 @@ async function run() {
     const myToysCollection = client
       .db("toysMarketPlaceDB")
       .collection("myToys");
+
+    //jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      res.send({ token });
+    });
 
     app.get("/toys", async (req, res) => {
       const toys = toyCollection.find();
@@ -51,8 +77,34 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedToy = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          seller: updatedToy.seller,
+          seller_email: updatedToy.seller_email,
+          name: updatedToy.name,
+          picture: updatedToy.picture,
+          price: updatedToy.price,
+          category: updatedToy.category,
+          subcategory: updatedToy.subcategory,
+          rating: updatedToy.rating,
+          quantity: updatedToy.quantity,
+          description: updatedToy.description,
+        },
+      };
+      const result = await toyCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    });
+
     app.post("/addAToy", async (req, res) => {
       const addedToy = req.body;
+      console.log(addedToy);
+      console.log(addedToy.seller_email);
       const result = await toyCollection.insertOne(addedToy);
       res.send(result);
     });
@@ -63,7 +115,7 @@ async function run() {
       const toys = toyCollection.find();
       const result = await toys.toArray();
       const categories = result.map((obj) => {
-        const category = obj.categories;
+        const category = obj.category;
         if (!seenCategories.has(category)) {
           uniqueCategories.push(category);
           seenCategories.add(category);
@@ -96,18 +148,25 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/myToys", async (req, res) => {
-      const myToys = myToysCollection.find();
-      const result = await myToys.toArray();
+    app.get("/myToys", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: "forbidden access" });
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { seller_email: req.query.email };
+      }
+      const result = await toyCollection.find(query).toArray();
       res.send(result);
     });
 
     app.delete("/myToys/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = {_id : id}
+      const query = { _id: id };
       const result = await myToysCollection.deleteOne(query);
-      res.send(result);      
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
